9d916734e86b03ee696a6410c3b6400eb02d5426
Gonsalves, Samuel David <samuel.gonsalves@inmar.com>
2019-10-18 10:27:10 -0400

    Use cards for personal projects

--- src/components/PersonalProjects.vue
+++ src/components/PersonalProjects.vue
@@ -7,58 +7,46 @@
                 I love tinkering and building things with software! 
             </h6>
             <b-row>
-                <h6><strong>Game Engine</strong></h6>
+                <b-col>
+                    <b-card>
+                        <h6><strong>Game Engine</strong></h6>
+                        <h6>Built a game engine in Java with a game object model, multithreaded architecture, event management, scripting.</h6>
+                        <h6><u>Technologies</u>: Java, Processing Framework, JavaScript</h6>
+                        <a href="https://github.com/sdg123/game-engine">Code</a>
+                    </b-card>
+                </b-col>
+            </b-row>
+            <b-row>                
+                <b-col>
+                    <b-card>
+                        <h6><strong>Bank app</strong></h6>
+                        <h6>Developed an MVC application in Ruby on Rails emulating the functionality of an online banking system.</h6>
+                        <h6><u>Technologies</u>: Ruby on Rails, PostgreSQL, JavaScript</h6>
+                        <a href="https://github.com/sdg123/bank-app">Code</a>
+                    </b-card>
+                </b-col>
             </b-row>
             <b-row>
-                <h6>Built a game engine in Java with a game object model, multithreaded architecture, event management, scripting.</h6>
+                <b-col>
+                    <b-card>
+                        <h6><strong>Continuous Integration Slack Bot</strong></h6>
+                        <h6>Built a Slack bot to interface between Travis CI, Coveralls and GitHub.</h6>
+                        <h6><u>Technologies</u>: Node.js, APIs: Travis CI, Coveralls, Slack, GitHub</h6>
+                        <a href="https://github.com/CiBotProject/bot">Code</a>
+                    </b-card>
+                </b-col>
             </b-row>
             <b-row>
-                <h6><u>Technologies</u>: Java, Processing Framework, JavaScript</h6>
-            </b-row>
-            <b-row>
-                <a href="https://github.com/sdg123/game-engine">Code</a>
-            </b-row>
-            <b-row>
-                <h6><strong>Bank app</strong></h6>
-            </b-row>
-            <b-row>
-                <h6>Developed an MVC application in Ruby on Rails emulating the functionality of an online banking system.</h6>
-            </b-row>
-            <b-row>
-                <h6><u>Technologies</u>: Ruby on Rails, PostgreSQL, JavaScript</h6>
-            </b-row>
-            <b-row>
-                <a href="https://github.com/sdg123/bank-app">Code</a>
-            </b-row>
-            <b-row>
-                <h6><strong>Continuous Integration Slack Bot</strong></h6>
-            </b-row>
-            <b-row>
-                <h6>Built a Slack bot to interface between Travis CI, Coveralls and GitHub.</h6>
-            </b-row>
-             <b-row>
-                <h6><u>Technologies</u>: Node.js, APIs: Travis CI, Coveralls, Slack, GitHub</h6>
-            </b-row>
-            <b-row>
-                <a href="https://github.com/CiBotProject/bot">Code</a>
-            </b-row>
-            <b-row>
-                <h6><strong>Open Source Contributions</strong></h6>
-            </b-row>
-            <b-row>
-                <h6>Fixed UI bugs in TheOdinProject web application.</h6>
-            </b-row>
-            <b-row>
-                <h6>Contributed to the open source project FreeCodeCamp.</h6>
-            </b-row>
-            <b-row>
-                <h6>Researched and added content to Wikipedia's Jasmine testing framework article.</h6>
-            </b-row>
-            <b-row>
-                <a href="https://tools.wmflabs.org/sigma/usersearch.py?name=Samuelg123&page=Jasmine_%28JavaScript_testing_framework%29&server=enwiki&max=">Wikipedia Contributions</a>
-            </b-row>
-            <b-row>
-                <h6>Added survey functionality to the open source project Expertiza to allow instructors to administer surveys for students.</h6>
+                <b-col>
+                    <b-card>
+                        <h6><strong>Open Source Contributions</strong></h6>
+                        <h6>Fixed UI bugs in TheOdinProject web application.</h6>
+                        <h6>Contributed to the open source project FreeCodeCamp.</h6>
+                        <h6>Researched and added content to Wikipedia's Jasmine testing framework article.</h6>
+                        <a href="https://tools.wmflabs.org/sigma/usersearch.py?name=Samuelg123&page=Jasmine_%28JavaScript_testing_framework%29&server=enwiki&max=">Wikipedia Contributions</a>
+                        <h6>Added survey functionality to the open source project Expertiza to allow instructors to administer surveys for students.</h6>
+                    </b-card>
+                </b-col>
             </b-row>
         </b-col>
     </b-row>
