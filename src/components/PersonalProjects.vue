40f3c9de3ea3a97e9a1c7ff78f4130d3931f6a29
Samuel Gonsalves <sgonsal@ncsu.edu>
2019-10-19 12:24:15 -0400

    Use pill buttons for links to code

--- src/components/PersonalProjects.vue
+++ src/components/PersonalProjects.vue
@@ -12,7 +12,7 @@
                         <h6><strong>Date Recommender App</strong></h6>
                         <h6>Currently building a web app to recommend date ideas by connecting to Yelp, Trello and other cool APIs.</h6>
                         <h6><u>Technologies</u>: Vue.js</h6>
-                        <a href="https://github.com/sdg123/date-recommender">Code</a>
+                        <b-button pill variant="outline-primary" href="https://github.com/sdg123/date-recommender">Code</b-button>
                     </b-card>
                 </b-col>
                 <b-col>
@@ -20,7 +20,7 @@
                         <h6><strong>Game Engine</strong></h6>
                         <h6>Built a game engine in Java with a game object model, multithreaded architecture, event management, scripting.</h6>
                         <h6><u>Technologies</u>: Java, Processing Framework, JavaScript</h6>
-                        <a href="https://github.com/sdg123/game-engine">Code</a>
+                        <b-button pill variant="outline-primary" href="https://github.com/sdg123/game-engine">Code</b-button>
                     </b-card>
                 </b-col>
             </b-row>
@@ -30,7 +30,7 @@
                         <h6><strong>Bank app</strong></h6>
                         <h6>Developed an MVC application in Ruby on Rails emulating the functionality of an online banking system.</h6>
                         <h6><u>Technologies</u>: Ruby on Rails, PostgreSQL, JavaScript</h6>
-                        <a href="https://github.com/sdg123/bank-app">Code</a>
+                        <b-button pill variant="outline-primary" href="https://github.com/sdg123/bank-app">Code</b-button>
                     </b-card>
                 </b-col>
                 <b-col>
@@ -38,7 +38,7 @@
                         <h6><strong>Continuous Integration Slack Bot</strong></h6>
                         <h6>Built a Slack bot to interface between Travis CI, Coveralls and GitHub.</h6>
                         <h6><u>Technologies</u>: Node.js, APIs: Travis CI, Coveralls, Slack, GitHub</h6>
-                        <a href="https://github.com/CiBotProject/bot">Code</a>
+                        <b-button pill variant="outline-primary" href="https://github.com/CiBotProject/bot">Code</b-button>
                     </b-card>
                 </b-col>
             </b-row>
@@ -49,8 +49,8 @@
                         <h6>Fixed UI bugs in TheOdinProject web application.</h6>
                         <h6>Contributed to the open source project FreeCodeCamp.</h6>
                         <h6>Researched and added content to Wikipedia's Jasmine testing framework article.</h6>
-                        <a href="https://tools.wmflabs.org/sigma/usersearch.py?name=Samuelg123&page=Jasmine_%28JavaScript_testing_framework%29&server=enwiki&max=">Wikipedia Contributions</a>
                         <h6>Added survey functionality to the open source project Expertiza to allow instructors to administer surveys for students.</h6>
+                        <b-button pill variant="outline-primary" href="https://tools.wmflabs.org/sigma/usersearch.py?name=Samuelg123&page=Jasmine_%28JavaScript_testing_framework%29&server=enwiki&max=">Wikipedia Contributions</b-button>
                     </b-card>
                 </b-col>
             </b-row>
@@ -58,6 +58,3 @@
     </b-row>
 </b-container>
 </template>
-
-<style scoped>
-</style>
