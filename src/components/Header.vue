c57f238b0ad6d7e8c1af17d9819bdd437630633f
Samuel Gonsalves <sgonsal@ncsu.edu>
2019-11-12 17:10:42 -0500

    Fix navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
-    <b-navbar pills variant="dark">
+    <b-navbar toggleable="lg" pills variant="dark">
         <b-navbar-brand href="#main">Samuel</b-navbar-brand>
-        <b-nav-item href="#aboutme">About Me</b-nav-item>
-        <b-nav-item href="#skills">Skills</b-nav-item>
-        <b-nav-item href="#experience">Experience</b-nav-item>
-        <b-nav-item href="#personalprojects">Projects</b-nav-item>
-        <b-nav-item href="#contact">Contact</b-nav-item>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-nav-item href="#aboutme">About Me</b-nav-item>
+            <b-nav-item href="#skills">Skills</b-nav-item>
+            <b-nav-item href="#experience">Experience</b-nav-item>
+            <b-nav-item href="#personalprojects">Projects</b-nav-item>
+            <b-nav-item href="#contact">Contact</b-nav-item>
+        </b-collapse>
     </b-navbar>
 </template>
 
